d112a2f5be1fc07afbdd480888b31115a06e23a9
ClarissaBaciu <clarissa.baciu@mail.mcgill.ca>
2022-02-20 12:55:25 -0500

    added spring project

---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/GroceryStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.GroceryStoreBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryStoreBackendApplication.class, args);
+	}
+
+}
