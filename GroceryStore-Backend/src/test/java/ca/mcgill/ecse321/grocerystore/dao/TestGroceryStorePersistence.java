81c1bae126987d4766bf3d295ee22c2702199616
ClarissaBaciu <clarissa.baciu@mail.mcgill.ca>
2022-02-22 23:44:05 -0500

    added first persistence test

--- GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/grocerystore/dao/TestGroceryStorePersistence.java
+++ GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/grocerystore/dao/TestGroceryStorePersistence.java
@@ -60,102 +60,53 @@ public class TestGroceryStorePersistence {
 	private StoreScheduleRepository storeSchedule;
 
 
-	// @AfterEach
-	// public void clearDatabase() {
-	// 	// clear to avoid exceptions due to inconsistencies (dependencies)
-	// 	itemRepository.deleteAll();
-	// 	deliveryOrderRepository.deleteAll();
-	// 	inStoreRepository.deleteAll();
-	// 	pickupOrderRepository.deleteAll();
-	// 	orderRepository.deleteAll();
-	// 	employeeScheduleRepository.deleteAll();
-	// 	customerRepository.deleteAll();
-	// 	employeeRepository.deleteAll();
-	// 	ownerRepository.deleteAll();
-	// 	accountRepository.deleteAll();
-	// 	inventoryItemRepository.deleteAll();
-	// 	storeSchedule.deleteAll();
-	// 	groceryStoreRepository.deleteAll();
-	// }
+	@AfterEach
+	public void clearDatabase() {
+		// clear to avoid exceptions due to inconsistencies (dependencies)
+		itemRepository.deleteAll();
+		deliveryOrderRepository.deleteAll();
+		inStoreRepository.deleteAll();
+		pickupOrderRepository.deleteAll();
+		groceryOrderRepository.deleteAll();
+		employeeScheduleRepository.deleteAll();
+		customerRepository.deleteAll();
+		employeeRepository.deleteAll();
+		ownerRepository.deleteAll();
+		accountRepository.deleteAll();
+		inventoryItemRepository.deleteAll();
+		storeSchedule.deleteAll();
+		groceryStoreRepository.deleteAll();
+	}
     
 	// //test persistence of GroceryStore class
-	
-
+	//test persistence of GroceryStore class
 	// @Test
-	// public void testPersistAndLoadPerson() {
-	// 	String name = "TestPerson";
-	// 	// First example for object save/load
-	// 	Person person = new Person();
-	// 	// First example for attribute save/load
-	// 	person.setName(name);
-	// 	personRepository.save(person);
+	// @Transactional
+	// public void testPersistAndLoadGroceryStore(){
+	// 	GroceryStore groceryStore = new GroceryStore();
 
-	// 	person = null;
-
-	// 	person = personRepository.findPersonByName(name);
-	// 	assertNotNull(person);
-	// 	assertEquals(name, person.getName());
 	// }
 
-	// @Test
-	// public void testPersistAndLoadEvent() {
-	// 	String name = "ECSE321 Tutorial";
-	// 	Date date = java.sql.Date.valueOf(LocalDate.of(2020, Month.JANUARY, 31));
-	// 	Time startTime = java.sql.Time.valueOf(LocalTime.of(11, 35));
-	// 	Time endTime = java.sql.Time.valueOf(LocalTime.of(13, 25));
-	// 	Event event = new Event();
-	// 	event.setName(name);
-	// 	event.setDate(date);
-	// 	event.setStartTime(startTime);
-	// 	event.setEndTime(endTime);
-	// 	eventRepository.save(event);
+	//test Customer persistence
+	@Test
+	public void testPersistAndLoadCustomer(){
+		Customer customer = new Customer();
+		String email = "testEmail";
 
-	// 	event = null;
+		customer.setName("testName");
+		customer.setEmail(email);
+		customer.setPassword("testPassword");
+		customer.setUsername("testUsername");
+		customer.setPhoneNumber("testPhoneNumber");
+		customerRepository.save(customer);
+		customer=null;
 
-	// 	event = eventRepository.findEventByName(name);
+		customer = customerRepository.findByEmail(email);
+		assertNotNull(customer);
+		assertEquals(email, customer.getEmail());
 
-	// 	assertNotNull(event);
-	// 	assertEquals(name, event.getName());
-	// 	assertEquals(date, event.getDate());
-	// 	assertEquals(startTime, event.getStartTime());
-	// 	assertEquals(endTime, event.getEndTime());
-	// }
+	}
 
-	// @Test
-	// public void testPersistAndLoadRegistration() {
-	// 	String personName = "TestPerson";
-	// 	Person person = new Person();
-	// 	person.setName(personName);
-	// 	personRepository.save(person);
-
-	// 	String eventName = "ECSE321 Tutorial";
-	// 	Date date = java.sql.Date.valueOf(LocalDate.of(2020, Month.JANUARY, 31));
-	// 	Time startTime = java.sql.Time.valueOf(LocalTime.of(11, 35));
-	// 	Time endTime = java.sql.Time.valueOf(LocalTime.of(13, 25));
-	// 	Event event = new Event();
-	// 	event.setName(eventName);
-	// 	event.setDate(date);
-	// 	event.setStartTime(startTime);
-	// 	event.setEndTime(endTime);
-	// 	eventRepository.save(event);
-
-	// 	Registration reg = new Registration();
-	// 	int regId = 1;
-	// 	// First example for reference save/load
-	// 	reg.setId(regId);
-	// 	reg.setPerson(person);
-	// 	reg.setEvent(event);
-	// 	registrationRepository.save(reg);
-
-	// 	reg = null;
-
-	// 	reg = registrationRepository.findByPersonAndEvent(person, event);
-	// 	assertNotNull(reg);
-	// 	assertEquals(regId, reg.getId());
-	// 	// Comparing by keys
-	// 	assertEquals(person.getName(), reg.getPerson().getName());
-	// 	assertEquals(event.getName(), reg.getEvent().getName());
-	// }
 
 }
 
