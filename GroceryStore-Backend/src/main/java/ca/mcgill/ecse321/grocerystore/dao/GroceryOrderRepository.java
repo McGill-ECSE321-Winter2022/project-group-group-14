abc71763930747bcf2ba4b028eea5076fe4f38f1
ClarissaBaciu <clarissa.baciu@mail.mcgill.ca>
2022-03-07 12:15:51 -0500

    modified grocery order repository

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/grocerystore/dao/GroceryOrderRepository.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/grocerystore/dao/GroceryOrderRepository.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse321.grocerystore.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.grocerystore.model.GroceryOrder;
+import ca.mcgill.ecse321.grocerystore.model.GroceryOrder.OrderType;
 import ca.mcgill.ecse321.grocerystore.model.Customer;
 import ca.mcgill.ecse321.grocerystore.model.OrderItem;
 
@@ -9,7 +10,10 @@ import java.util.List;
 
 public interface GroceryOrderRepository extends CrudRepository <GroceryOrder, Integer>{
     GroceryOrder findByOrderId(int orderId);
+    List<GroceryOrder> findByOrderType(OrderType orderType);
     List<GroceryOrder> findGroceryOrdersByCustomer(Customer customer);
     GroceryOrder findGroceryOrderByOrderItems(OrderItem orderItems);
+    //GroceryOrder findGroceryOrderByOrderItems(OrderItem orderItems); //not very important
+    //possibly add find order items by boolean if its paied or not
 }
 
