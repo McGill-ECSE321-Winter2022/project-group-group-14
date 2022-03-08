66dbd176fa1d95b1eb77951807ff8bc744f63122
Mos2d <90064094+Mos2d@users.noreply.github.com>
2022-03-08 15:46:36 -0500

    testing all queries for order and inventory item

--- GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/grocerystore/dao/TestOrderItemPersistence.java
+++ GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/grocerystore/dao/TestOrderItemPersistence.java
@@ -2,7 +2,8 @@ package ca.mcgill.ecse321.grocerystore.dao;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.AfterEach;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import ca.mcgill.ecse321.grocerystore.model.Customer;
 import ca.mcgill.ecse321.grocerystore.model.GroceryOrder;
-import ca.mcgill.ecse321.grocerystore.model.GroceryStore;
 import ca.mcgill.ecse321.grocerystore.model.OrderItem;
 
 @ExtendWith(SpringExtension.class)
@@ -58,54 +57,51 @@ public class TestOrderItemPersistence {
 //    
 
 	//test Inventory Item persistence
+	/**@author Youssof Mohamed */
 	@Test
 	public void testPersistAndLoadOrderItem(){
 
-		//create Grocery Order object
-		GroceryOrder groceryOrder = new GroceryOrder();
-		
-//		groceryOrder.setCustomer(new Customer());		
-		//set values to Order Item attributes
+				
+		//set values to the first Order Item attributes
 		OrderItem orderItem = new OrderItem();
-		String name = "btates";
+		String name = "batates";
 		orderItem.setName(name);
 		orderItem.setPrice(32);
 		orderItem.setCurrentStock(15);
 		
-//		orderItem.setGroceryStore(groceryStore);
-//		orderItem.setGroceryOrder(groceryOrder);
-
+		//set values to the second Order Item attributes
+		OrderItem orderItem2 = new OrderItem();
+		orderItem2.setName("tamatem");
+		orderItem2.setPrice(123);
+		orderItem2.setCurrentStock(74);
 		
+		//set values to the second Order Item attributes (same name as in second order item)
+		OrderItem orderItem3 = new OrderItem();
+		orderItem3.setName("tamatem");
+		orderItem3.setPrice(123);
+		orderItem3.setCurrentStock(73);
 		
-		//save Order Item object
-//		orderItemRepository.save(orderItem);
-		
-		orderItem = orderItemRepository.save(orderItem);
-		
-		//save Grocery Order object
-		groceryOrder=groceryOrderRepository.save(groceryOrder);
-		
-		
-		
-		System.out.println(orderItem.getItemId());
-		
-
-		
-		//save Order Item object
+		//save Order Item objects
 		orderItemRepository.save(orderItem);
-		
-		//save Grocery Order object
-		groceryOrderRepository.save(groceryOrder);
-		
-			
-		
-		
-		//test saved Order Item object
+		orderItemRepository.save(orderItem2);
+		orderItemRepository.save(orderItem3);
+	
+		//test findByItemId
 		OrderItem savedOrderItem = orderItemRepository.findByItemId(orderItem.getItemId());
 		assertNotNull(savedOrderItem);
 		assertEquals(orderItem.getName(), savedOrderItem.getName());
 		assertEquals(orderItem.getPrice(), savedOrderItem.getPrice());
 		assertEquals(orderItem.getCurrentStock(), savedOrderItem.getCurrentStock());
+		
+		//test findByName
+		List<OrderItem> savedOrderItems = orderItemRepository.findByName(orderItem2.getName());
+		assertNotNull(savedOrderItems);
+		assertEquals(orderItem2.getItemId(), savedOrderItems.get(0).getItemId());
+		assertEquals(orderItem2.getPrice(), savedOrderItems.get(0).getPrice());
+		assertEquals(orderItem2.getCurrentStock(), savedOrderItems.get(0).getCurrentStock());
+		assertEquals(orderItem3.getItemId(), savedOrderItems.get(1).getItemId());
+		assertEquals(orderItem3.getPrice(), savedOrderItems.get(1).getPrice());
+		assertEquals(orderItem3.getCurrentStock(), savedOrderItems.get(1).getCurrentStock());
 		
 	}
 
