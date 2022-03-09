061bb909e16e543db3da637f08e47f5716bc4ad8
minharumin <minkyu.park@mail.mcgill.ca>
2022-03-08 21:59:48 -0500

    employee schedule persistance

---
+++ GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/grocerystore/dao/TestEmployeeSchedulePersistence.java
@@ -0,0 +1,71 @@
+package ca.mcgill.ecse321.grocerystore.dao;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.grocerystore.model.GroceryStore;
+import ca.mcgill.ecse321.grocerystore.model.StoreSchedule;
+import ca.mcgill.ecse321.grocerystore.model.Customer;
+import ca.mcgill.ecse321.grocerystore.model.EmployeeSchedule;
+import ca.mcgill.ecse321.grocerystore.model.EmployeeSchedule.Day;
+import ca.mcgill.ecse321.grocerystore.model.EmployeeSchedule.Shift;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional //don't need clear database method
+public class TestEmployeeSchedulePersistence {
+    @Autowired
+	private AccountRepository accountRepository;
+	@Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Autowired
+	private EmployeeScheduleRepository employeeScheduleRepository;
+	@Autowired
+	private GroceryStoreRepository groceryStoreRepository;
+	@Autowired
+	private InventoryItemRepository inventoryItemRepository;
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	@Autowired
+	private GroceryOrderRepository groceryOrderRepository;
+	@Autowired
+	private OwnerRepository ownerRepository;
+	@Autowired
+	private StoreScheduleRepository storeSchedule;
+	
+	//test EmployeeSchedule persistence
+	@Test
+	public void testPersistAndLoadEmployeeSchedule(){
+		
+		EmployeeSchedule employeeSchedule = new EmployeeSchedule(null, null);
+		
+		//set employee schedule variables for test
+		Shift ashift = Shift.Morning;
+		Day aday = Day.Sunday;
+		
+		//set values to employee schedule
+		employeeSchedule.setShift(ashift);
+		employeeSchedule.setDay(aday);
+		
+		//save employee schedule object
+		employeeScheduleRepository.save(employeeSchedule);
+		
+		//test saved Employee Schedule object
+		EmployeeSchedule savedEmployeeSchedule = employeeScheduleRepository.findById(employeeSchedule.getId());
+		
+		assertNotNull(savedEmployeeSchedule);
+		assertEquals(employeeSchedule.getShift(), savedEmployeeSchedule.getShift());
+		assertEquals(employeeSchedule.getDay(), savedEmployeeSchedule.getDay());
+	
+	}
+	
+}	
