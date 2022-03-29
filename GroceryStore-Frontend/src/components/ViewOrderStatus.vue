b768799127ddeaa53499b8c3d679a4336e24c1b9
ClarissaBaciu <clarissa.baciu@mail.mcgill.ca>
2022-03-29 16:51:26 -0400

    added view order status page

---
+++ GroceryStore-Frontend/src/components/ViewOrderStatus.vue
@@ -0,0 +1,18 @@
+<template>
+    <div >
+        <ul class="navBar">
+            <li class="logo">STORIKO</li>   
+            <li class="page"><a href="#/">Log Out</a></li>     
+        </ul>
+
+        <div class="verticalandhorizontal-center">
+            <h2 class="heading">Order Status </h2>
+                <br>
+                <h4> We are carefully preparing your order! </h4>
+                <br>
+                <h4> nice little illustration of the order status </h4>
+
+
+        </div>
+    </div>
+</template>
