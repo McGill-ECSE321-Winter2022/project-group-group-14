98b12d3c5ba342bdfc074eedf6f0f3628034e956
ClarissaBaciu <clarissa.baciu@mail.mcgill.ca>
2022-03-29 19:02:33 -0400

    fixed nav bar

--- GroceryStore-Frontend/src/components/ViewCart.vue
+++ GroceryStore-Frontend/src/components/ViewCart.vue
@@ -1,11 +1,18 @@
 <template>
     <div >
-    <ul class="navBar">
-        <li class="logo">STORIKO</li>
-        <li class="page"><a href="#/">Log Out</a></li>  
-        <li class="page"><a href="#/viewCart">View Cart</a></li>
-        <li class="page"><a href="#/showCustomerInventoryItems">View Items</a></li>
-    </ul>
+  <b-navbar fixed="top" toggleable="lg">
+      <b-navbar-brand>STORIKO</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#/showCustomerInventoryItems">View Items</b-nav-item>
+            <b-nav-item href="#/viewCart">View Cart</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#/">Log Out</b-nav-item>
+          </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
 
         <div class="verticalandhorizontal-center">
             <h2 class="heading">Cart</h2>
