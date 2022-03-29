1d9d1c529dccef156351032be2d1e61e279cf718
ClarissaBaciu <clarissa.baciu@mail.mcgill.ca>
2022-03-29 18:03:59 -0400

    added most templates

--- GroceryStore-Frontend/src/components/Report.vue
+++ GroceryStore-Frontend/src/components/Report.vue
@@ -13,8 +13,6 @@
         <li class="page"><a href="#/deleteOwnerAccount">Delete Account</a></li> 
         <li class="page"><a href="#/">Log Out</a></li>  
   </ul>
-   <h2 class="heading">Report</h2>
-
   </div>
 </template>
 
