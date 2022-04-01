9eef11846a454e268e5f0806884ccf671ca34192
yakirbender <yakirbender@gmail.com>
2022-04-01 12:21:20 -0400

    a

--- GroceryStore-Frontend/src/components/owner/StoreScheduleOwner.js
+++ GroceryStore-Frontend/src/components/owner/StoreScheduleOwner.js
@@ -9,7 +9,7 @@ export default {
 name: 'storeschedule',
 data () {
     return {
-    storeschedules: [],
+    storeSchedules: [],
     newStoreSchedule: '',
     errorSchedule: '',
     response: []
