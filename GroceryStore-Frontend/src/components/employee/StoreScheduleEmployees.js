8d9a0b6281c4e2cd4404d1697534dab1924aa6a8
yakirbender <yakirbender@gmail.com>
2022-04-03 13:15:24 -0400

    finished other schedule pages

---
+++ GroceryStore-Frontend/src/components/employee/StoreScheduleEmployees.js
@@ -0,0 +1,91 @@
+import axios from 'axios'
+var config = require('../../../config')
+
+var frontendUrl = process.env.FRONTEND_HOST + ':' + process.env.FRONTEND_PORT
+var backendUrl = process.env.BACKEND_HOST + ':' + process.env.BACKEND_PORT
+
+// var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+// var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})  
+  
+
+export default {
+name: 'storeschedule',
+data () {
+    return {
+    storeSchedules: [],
+    newStoreSchedule: {
+        openingTime: '',
+        closingTime: '',
+        dayOpen: ''
+      },
+    errorSchedule: '',
+    response: []
+    }
+},
+
+created: function () {
+    // Initializing schedules from backend
+        AXIOS.get('/storeSchedules/get')
+        .then(response => {
+            // JSON responses are automatically parsed.
+            this.storeSchedules = response.data
+        })
+        .catch(e => {
+            this.errorSchedule = e
+        })
+        
+    },
+
+methods: {
+    createStoreSchedule: function (openTime,closeTime,day) {
+        AXIOS.post('/storeSchedules/create/'.concat(day), {}, {params: {openingTime: openTime, closingTime: closeTime}})
+          .then(response => {
+          // JSON responses are automatically parsed.
+            this.storeSchedules.push(response.data)
+            this.errorSchedule = ''
+            this.newStoreSchedule = ''
+          })
+          .catch(e => {
+            var errorMsg = e.response.data
+            console.log(errorMsg)
+            this.errorSchedule = errorMsg
+          })
+      },
+      updateStoreSchedule: function (openTime,closeTime,day) {
+        AXIOS.put('/storeSchedules/update/'.concat(day), {}, {params: {params: {openingTime: openTime, closingTime: closeTime}}})
+          .then(response => {
+          // JSON responses are automatically parsed.
+            this.storeSchedules.push(response.data)
+            this.errorSchedule = ''
+            this.newStoreSchedule = ''
+          })
+          .catch(e => {
+            var errorMsg = e.response.data
+            console.log(errorMsg)
+            this.errorSchedule = errorMsg
+          })
+      },
+      deleteStoreSchedule: function (day) {
+        AXIOS.delete('/storeSchedules/delete/'.concat(day), {}, {})
+          .then(response => {
+          // JSON responses are automatically parsed.
+            // this.storeSchedules
+            this.errorSchedule = ''
+            this.newStoreSchedule = ''
+          })
+          .catch(e => {
+            var errorMsg = e.response.data
+            console.log(errorMsg)
+            this.errorSchedule = errorMsg
+          })
+      }
+
+
+}
+//...
+}
