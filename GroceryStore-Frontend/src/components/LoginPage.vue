40f372f8cd7f5007e0241907d8ab5f528eca3b2c
SamuelValentine <samuel.valentine@mail.mcgill.ca>
2022-04-02 22:13:38 -0400

    adding login functionality

--- GroceryStore-Frontend/src/components/LoginPage.vue
+++ GroceryStore-Frontend/src/components/LoginPage.vue
@@ -1,10 +1,28 @@
 <template>
 <div>
+        <div id="popup1" class="overlay" v-if="successCustomer">
+          <div class="popup">
+            <h5>{{ successCustomer }}</h5>
+            <router-link :to="{ name: 'customerWelcomePage', params: { email: account.email }}">
+            <button class="mediumButton">Login</button>
+            </router-link>
+          </div>
+        </div>
+
         <div id="popup1" class="overlay" v-if="errorCustomer">
           <div class="popup">
             <h5>{{ errorCustomer }}</h5>
-            <router-link :to="{ name: 'customerWelcomePage', params: { email: account.email }}">
-            <button class="mediumButton">Close</button>
+            <router-link to="/">    
+            <button class="mediumButton" onClick="window.location.reload();">Close</button>
+            </router-link> 
+          </div>
+        </div>
+
+        <div id="popup1" class="overlay" v-if="successEmployee">
+          <div class="popup">
+            <h5>{{ successEmployee }}</h5>
+            <router-link :to="{ name: 'employeeWelcomePage', params: { email: account.email }}">
+            <button class="mediumButton">Login</button>
             </router-link>
           </div>
         </div>
@@ -12,8 +30,17 @@
         <div id="popup1" class="overlay" v-if="errorEmployee">
           <div class="popup">
             <h5>{{ errorEmployee }}</h5>
-            <router-link :to="{ name: 'employeeWelcomePage', params: { email: account.email }}">
-            <button class="mediumButton">Close</button>
+            <router-link to="/">    
+            <button class="mediumButton" onClick="window.location.reload();">Close</button>
+            </router-link> 
+          </div>
+        </div>
+
+        <div id="popup1" class="overlay" v-if="successOwner">
+          <div class="popup">
+            <h5>{{ successOwner }}</h5>
+            <router-link :to="{ name: 'ownerWelcomePage', params: { email: account.email }}">
+            <button class="mediumButton">Login</button>
             </router-link>
           </div>
         </div>
@@ -21,9 +48,9 @@
         <div id="popup1" class="overlay" v-if="errorOwner">
           <div class="popup">
             <h5>{{ errorOwner }}</h5>
-            <router-link :to="{ name: 'ownerWelcomePage', params: { email: account.email }}">
-            <button class="mediumButton">Close</button>
-            </router-link>
+            <router-link to="/">    
+            <button class="mediumButton" onClick="window.location.reload();">Close</button>
+            </router-link> 
           </div>
         </div>
 
@@ -92,28 +119,40 @@
           <br> -->
 
 
-                <!-- <button v-bind:disabled="!account.email || !account.password" class="mediumButton" @click="loginCustomer(account.email,account.password)">
+              <button v-bind:disabled="!account.email || !account.password" class="mediumButton" @click="loginCustomer(account.email,account.password)">
                 
-                  Customer Log In
+                Customer Log In
 
-                </button> -->
+              </button>
 
 
-              <router-link :to="{ name: 'customerWelcomePage', params: { email: account.email }}">
+              <!-- <router-link :to="{ name: 'customerWelcomePage', params: { email: account.email }}">
                 <button v-bind:disabled="!account.email || !account.password" class="mediumButton">
                   Customer Log In
                 </button>
-              </router-link>
+              </router-link> -->
 
               <br>
 
-              <router-link :to="{ name: 'employeeWelcomePage', params: { email: account.email }}">
+              <button v-bind:disabled="!account.email || !account.password" class="mediumButton" @click="loginEmployee(account.email,account.password)">
+                
+                Employee Log In
+
+              </button>
+
+              <!-- <router-link :to="{ name: 'employeeWelcomePage', params: { email: account.email }}">
                 <button v-bind:disabled="!account.email || !account.password" class="mediumButton">
                   Employee Log In
                 </button>
-              </router-link>
+              </router-link> -->
 
               <br>
+
+              <!-- <button v-bind:disabled="!account.email || !account.password" class="mediumButton" @click="loginCustomer(account.email,account.password)">
+                
+                Owner Log In
+
+              </button> -->
 
               <router-link :to="{ name: 'ownerWelcomePage', params: { email: account.email }}">
                 <button v-bind:disabled="!account.email || !account.password" class="mediumButton">
@@ -132,6 +171,10 @@
                   Create an account
                 </button>
               </router-link>
+
+              <button  type="CreateOwnerButton" class="button" @click="createOwner()">
+                Create owner
+              </button>
 
       </div>
     </div>
@@ -155,39 +198,86 @@ export default {
     data () {
         return {
             customers: [],
+            employees: [],
+            owners: [],
             account: {
                 email: '',
                 password: ''
             },
             errorCustomer: '',
+            successCustomer: '',
+            errorEmployee: '',
+            successEmployee: '',
+            errorOwner: '',
+            successOwner: '',
             reponse: []
         }
     },
 
-    // created: function(){
-    //     AXIOS.post('/customers/youssof@gmail.com/youssof5/123Abc/1111stavenue/5148888888')
-    //     .then(response => {
-    //         this.customers = response.data
-    //     })
-    //     .catch(e => {
-    //         this.errorCustomer = e
-    //     })
-    // },
+    created: function(){
+        AXIOS.post('/owners/youssof@gmail.com/youssof/123Abc')
+        .then(response => {
+          this.owners.push(response.data)
+        })
+        .catch(e => {
+            this.errorOwner = e
+        })
+    },
 
     methods: {
       loginCustomer: function (email, password){
+
         AXIOS.get('/customers/login/'.concat(email).concat("/").concat(password))
         .then(response => {
-           this.errorCustomer = newEmail + ' is logged in Successfully!'
+                this.successCustomer = email + ' is logged in Successfully!'
             })
             .catch(e => {
-                var errorMsg = e.response.data.message
+                var errorMsg = e.response.data
                 console.log(errorMsg)
-                this.errorCustomer = errorMsg
+                this.errorCustomer = e.response.data
             })
-        }
-      }
+        },
 
+      loginEmployee: function (email, password){
+
+        AXIOS.get('/employees/login/'.concat(email).concat("/").concat(password))
+        .then(response => {
+                this.successEmployee = email + ' is logged in Successfully!'
+            })
+            .catch(e => {
+                var errorMsg = e.response.data
+                console.log(errorMsg)
+                this.errorEmployee = e.response.data
+            })
+        },
+
+      loginOwner: function (email, password){
+
+        AXIOS.get('/owners/login/'.concat(email).concat("/").concat(password))
+        .then(response => {
+                this.successOwner = email + ' is logged in Successfully!'
+            })
+            .catch(e => {
+                var errorMsg = e.response.data
+                console.log(errorMsg)
+                this.errorOwner = e.response.data
+            })
+        },
+
+      // createOwner: function (){
+      //   AXIOS.post('/owners/youssof@gmail.com/youssof/123Abc')
+      //   .then(response => {
+      //           this.successOwner = email + ' is logged in Successfully!'
+      //       })
+      //       .catch(e => {
+      //           // var errorMsg = e.response.data
+      //           var errorMsg = "could not create owner"
+      //           console.log(errorMsg)
+      //           this.errorOwner = e.response.data
+      //       })
+      // }
+    
+    }
 }
 
 
@@ -196,6 +286,35 @@ export default {
 
 <style scoped>
   .verticalandhorizontal-center {
+    padding: 2% 6% 2% 6%;
+    background-color: white;
+    border-radius: 4%;
+    margin-top: 1%;
+    box-shadow: 0 0 10px 7px rgb(0,0,0,0.3);
+  }
+  
+  .overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.7);
+  transition: opacity 500ms;
+  opacity: 100%;
+  z-index: 100;
+}
+
+.popup {
+  margin: auto;
+  margin-top: 40vh;
+  padding: 20px;
+  background: #fff;
+  border-radius: 5px;
+  width: 30%;
+  transition: all 5s ease-in-out;
+}
+.verticalandhorizontal-center {
     padding: 2% 6% 2% 6%;
     background-color: white;
     border-radius: 4%;
