55fe07cf32da3f326726c6468f80e5cc00ab3d32
ClarissaBaciu <clarissa.baciu@mail.mcgill.ca>
2022-04-11 14:23:27 -0400

    almost got the correct picture showing for order status

--- GroceryStore-Frontend/src/components/customer/ThankYou.vue
+++ GroceryStore-Frontend/src/components/customer/ThankYou.vue
@@ -4,31 +4,35 @@
 
 
         <div class="verticalandhorizontal-center">
-            <h2 class="heading">Thank You for your order</h2>
+            <h2 class="heading">Thank You for your order!</h2>
             <br>
-            <br>
-            <div >
-                <img src="../../assets/status_d1.jpg" style = "width : 500px; heaight : 500px;">
-            </div>
 
-            <div v-if ="curremail">
+
+            <!-- <div v-if ="curremail">
                 Email : {{curremail}}
             </div>
 
             <div v-if ="orderId">
                 order Id : {{orderId}}
+            </div> -->
+
+
+            <div v-if = "imageUrl" >
+                <img :src="returnUrl()" style = "width : 500px; " >
+                <!-- <img :src=imageUrl style = "width : 500px; "> -->
+                <br>
+                IMAGE SHOULD BE HERE,  URL = :{{imageUrl}}
             </div>
 
-            <div v-if ="groceryOrders[0]">
-                Order Status : {{groceryOrders[0].orderStatus}}
-             </div>
-
-            <div v-if ="groceryOrders[0]">
-                 <div v-if ="groceryOrders[0].orderStatus === 'Received' ">
-                 We are processing!!!
-                </div>
-            </div>
             
+            <br>
+
+            <span v-if ="groceryOrders[0]">
+                {{returnStatus(groceryOrders[0])}}
+            </span>
+            <br>
+
+
 
 
             <br>
@@ -61,8 +65,6 @@ var AXIOS = axios.create({
 })  
 
 
-
-
 export default{
     name:'CustomerWelcomePage',
     components:{
@@ -73,7 +75,7 @@ export default{
         return {
             curremail : this.$route.params.email,
             orderId: this.$route.params.orderId,
-            ReceivedString : "Received",
+            imageUrl : "",
             groceryOrders :[],
             error: '',
             successMsg:'',
@@ -101,6 +103,44 @@ export default{
                 // this.errorInventory = e.response.data
                 // console.log(e.response.data)
             })
+    },
+    methods:{
+        returnStatus: function(order){
+            if (order.orderType == "Delivery"){ //order delivery pictures
+                if (order.orderStatus == "Received"){
+                    this.imageUrl = "../../assets/status_d1.jpg";
+                    return "Your order has been created, please add items and place it. Our employees can then start preparing it.";
+                }else if (order.orderStatus == "Processing"){
+                    this.imageUrl = "../../assets/status_d2.jpg";
+                    return "Your order is being prepared.";
+                }else if (order.orderStatus == "BeingDelivered"){
+                    this.imageUrl = "../../assets/status_d3.jpg";
+                    return "Your order is being delivered";
+                }else if (order.orderStatus == "Completed"){
+                    this.imageUrl = "../../assets/status_d4.jpg";
+                    return "Your latest order has been completed";
+                }
+            }else if(order.orderType == "PickUp") { //order is then pick up 
+                if (order.orderStatus == "Received"){
+                    this.imageUrl = "../../assets/status_p1.jpg";
+                    return "Your order has been created, please add items and place it. Our employees can then start preparing it.";
+                }else if (order.orderStatus == "Processing"){
+                    this.imageUrl = "../../assets/status_p2.jpg";
+                    return "Your order is being prepared.";
+                }else if (order.orderStatus == "ReadyForPickUp"){
+                    this.imageUrl = "../../assets/status_p3.jpg";
+                    return "Your order is ready for pick up.";
+                }else if (order.orderStatus == "Completed"){
+                    this.imageUrl = "../../assets/status_p4.jpg";
+                    return "Your latest order has been completed";
+                }
+            }
+            return "Could not read order status"; 
+        },
+        returnUrl: function(){
+            return this.imageUrl;
+        }
+
     }
 }
 </script>
